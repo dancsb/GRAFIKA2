b886aa59b713f8cfb74d6133fd5b698e28e84061
Dancs Balázs <100741583+dancsb@users.noreply.github.com>
2022-11-19 03:22:47 +0100

    spekuláris ambiens meg minden

--- src/Skeleton.cpp
+++ src/Skeleton.cpp
@@ -33,7 +33,6 @@
 //=============================================================================================
 #include "framework.h"
 
-const float epsilon = 0.0001f;
 float fov = 45 * M_PI / 180;
 vec3 eye = vec3(0, 0, 2), vup = vec3(0, 1, 0), lookat = vec3(0, 0, 0);
 
@@ -65,12 +64,6 @@ const char *fragmentSource = R"(
 	}
 )";
 
-struct Material {
-    vec3 ka, kd, ks;
-    float  shininess;
-    Material(vec3 _kd, vec3 _ks, float _shininess) : ka(_kd * M_PI), kd(_kd), ks(_ks) { shininess = _shininess; }
-};
-
 struct Ray {
     vec3 start, dir;
     Ray(vec3 _start, vec3 _dir) { start = _start; dir = normalize(_dir); }
@@ -79,17 +72,7 @@ struct Ray {
 struct Hit {
     float t;
     vec3 position, normal;
-    Material * material;
     Hit() { t = -1; }
-};
-
-struct Light {
-    vec3 direction;
-    vec3 Le;
-    Light(vec3 _direction, vec3 _Le) {
-        direction = normalize(_direction);
-        Le = _Le;
-    }
 };
 
 class Camera {
@@ -113,8 +96,6 @@ public:
 Camera camera;
 
 class Intersectable {
-protected:
-    Material * material;
 public:
     virtual Hit intersect(const Ray& ray) = 0;
 };
@@ -123,10 +104,9 @@ struct Sphere : public Intersectable {
     vec3 center;
     float radius;
 
-    Sphere(const vec3& _center, float _radius, Material* _material) {
+    Sphere(const vec3& _center, float _radius) {
         center = _center;
         radius = _radius;
-        material = _material;
     }
 
     Hit intersect(const Ray& ray) {
@@ -144,7 +124,6 @@ struct Sphere : public Intersectable {
         hit.t = (t2 > 0) ? t2 : t1;
         hit.position = ray.start + ray.dir * hit.t;
         hit.normal = (hit.position - center) * (1.0f / radius);
-        hit.material = material;
         return hit;
     }
 };
@@ -152,23 +131,20 @@ struct Sphere : public Intersectable {
 struct Triangle : public Intersectable {
     vec3 r1, r2 ,r3;
 
-    Triangle(const vec3& r1, const vec3& r2, const vec3& r3, Material* material) {
+    Triangle(const vec3& r1, const vec3& r2, const vec3& r3) {
         this->r1 = r1;
         this->r2 = r2;
         this->r3 = r3;
-        this->material = material;
     }
 
     Hit intersect(const Ray& ray) {
         Hit hit;
         hit.normal = cross((r2 - r1), (r3 - r1));
-        //printf("normal vector: x: %f, y: %f, z: %f", hit.normal.x, hit.normal.y, hit.normal.z);
         hit.t = dot((r1 - ray.start), hit.normal) / dot(ray.dir, hit.normal);
         hit.position = ray.start + ray.dir * hit.t;
         if (dot(cross((r2 - r1), (hit.position - r1)), hit.normal) <= 0.0f) hit.t = -1.0f;
         if (dot(cross((r3 - r2), (hit.position - r2)), hit.normal) <= 0.0f) hit.t = -1.0f;
         if (dot(cross((r1 - r3), (hit.position - r3)), hit.normal) <= 0.0f) hit.t = -1.0f;
-        hit.material = material;
         return hit;
     }
 };
@@ -177,19 +153,14 @@ float rnd() { return (float)rand() / RAND_MAX; }
 
 class Scene {
     std::vector<Intersectable *> objects;
-    std::vector<Light *> lights;
     vec3 La;
 public:
     void build() {
         La = vec3(0.0f, 0.0f, 0.0f);
-        vec3 lightDirection(1, 1, 1), Le(2, 2, 2);
-        lights.push_back(new Light(lightDirection, Le));
 
-        vec3 kd(0.3f, 0.0f, 0.0f), ks(2, 2, 2);
-        Material * material = new Material(kd, ks, 50);
         for (int i = 0; i < 15; i++)
-            objects.push_back(new Sphere(vec3(rnd() - 0.5f, rnd() - 0.5f, rnd() - 0.5f), rnd() * 0.1f, material));
-        objects.push_back(new Triangle(vec3(-0.5f, -0.5f, 0.5f), vec3(0.5f, -0.5f, 0.5f), vec3(0.0f, 0.5f, 0.0f), material));
+            objects.push_back(new Sphere(vec3(rnd() - 0.5f, rnd() - 0.5f, rnd() - 0.5f), rnd() * 0.1f));
+        objects.push_back(new Triangle(vec3(-0.5f, -0.5f, 0.5f), vec3(0.5f, -0.5f, 0.5f), vec3(0.0f, 0.5f, 0.0f)));
     }
 
     void render(std::vector<vec4>& image) {
@@ -205,32 +176,18 @@ private:
     Hit firstIntersect(Ray ray) {
         Hit bestHit;
         for (Intersectable * object : objects) {
-            Hit hit = object->intersect(ray); //  hit.t < 0 if no intersection
+            Hit hit = object->intersect(ray);
             if (hit.t > 0 && (bestHit.t < 0 || hit.t < bestHit.t))  bestHit = hit;
         }
         if (dot(ray.dir, bestHit.normal) > 0) bestHit.normal = bestHit.normal * (-1);
         return bestHit;
     }
 
-    bool shadowIntersect(Ray ray) {	// for directional lights
-        for (Intersectable * object : objects) if (object->intersect(ray).t > 0) return true;
-        return false;
-    }
-
-    vec3 trace(Ray ray, int depth = 0) {
+    vec3 trace(Ray ray) {
         Hit hit = firstIntersect(ray);
         if (hit.t < 0) return La;
-        vec3 outRadiance = hit.material->ka * La;
-        for (Light * light : lights) {
-            Ray shadowRay(hit.position + hit.normal * epsilon, light->direction);
-            float cosTheta = dot(hit.normal, light->direction);
-            if (cosTheta > 0 && !shadowIntersect(shadowRay)) {	// shadow computation
-                outRadiance = outRadiance + light->Le * hit.material->kd * cosTheta;
-                vec3 halfway = normalize(-ray.dir + light->direction);
-                float cosDelta = dot(hit.normal, halfway);
-                if (cosDelta > 0) outRadiance = outRadiance + light->Le * hit.material->ks * powf(cosDelta, hit.material->shininess);
-            }
-        }
+        float c = 0.2f * (1.0f + dot(hit.normal, vup));
+        vec3 outRadiance = vec3(c, c, c);
         return outRadiance;
     }
 };
@@ -238,7 +195,6 @@ private:
 class FullScreenTexturedQuad {
     unsigned int vao, vbo;
     Texture * texture;
-    float vertexCoords[8] = { -1, -1, 1, -1, 1, 1, -1, 1 };
 public:
     FullScreenTexturedQuad(int windowWidth, int windowHeight, std::vector<vec4>& image)
     {
@@ -249,6 +205,7 @@ public:
         glGenBuffers(1, &vbo);
 
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        float vertexCoords[] = { -1, -1, 1, -1, 1, 1, -1, 1 };
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertexCoords), vertexCoords, GL_STATIC_DRAW);
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);
@@ -269,9 +226,7 @@ Scene scene;
 FullScreenTexturedQuad * fullScreenTexturedQuad;
 
 void onInitialization() {
-    vec3 kd(0.3f, 0.0f, 0.0f), ks(2, 2, 2);
-    Material * material = new Material(kd, ks, 50);
-    Triangle ta = Triangle(vec3(-5.0f, -5.0f, -5.0f), vec3(5.0f, -5.0f, -5.0f), vec3(0.0f, 5.0f, 0.0f), material);
+    Triangle ta = Triangle(vec3(-5.0f, -5.0f, -5.0f), vec3(5.0f, -5.0f, -5.0f), vec3(0.0f, 5.0f, 0.0f));
     ta.intersect(Ray(eye, lookat-eye));
 
     glViewport(0, 0, windowWidth, windowHeight);
@@ -313,6 +268,6 @@ void onIdle() {
         scene.render(image);
         fullScreenTexturedQuad->setTexture(windowWidth, windowHeight, image);
         glutPostRedisplay();
-        fok += 10;
+        //fok += 10;
     }
 }
